8f8512fffa7d61689eed8aa450d29f672ec665e3
KyleDixonVO <KyleDixon@hotmail.ca>
2021-11-08 12:43:49 -0400

    Learned about lists, foreach loops

--- Prog1_Lists/Program.cs
+++ Prog1_Lists/Program.cs
@@ -10,6 +10,57 @@ namespace Prog1_Lists
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Arrays");
+            Console.WriteLine("(inventory1 System)");
+            Console.WriteLine();
+
+            string currentItem;
+            string[] inventory1 = new string[10];
+
+            currentItem = "health potion";
+            for (int i = 0; i <= inventory1.Length - 1; i++)
+            {
+                inventory1[i] = "";
+            }
+
+            inventory1[0] = "health potion"; // works but could overlap if zero is already taken
+
+
+            Console.WriteLine("Lists");
+            Console.WriteLine("(Inventory System)");
+            Console.WriteLine();
+
+            List<string> inventory = new List<string>();
+            // declaration
+            //                          instantiation
+
+            Console.WriteLine("intventory size = " + inventory.Count);
+            inventory.Add("health potion");
+            Console.WriteLine("intventory size = " + inventory.Count);
+            inventory.Add("health potion");
+            Console.WriteLine("intventory size = " + inventory.Count);
+            //shows inventory size (amount of items)
+
+            Console.WriteLine(inventory[1]);
+
+            for (int j = 0; j <= inventory.Count - 1; j++)
+            {
+                Console.WriteLine(inventory[j]);
+            }
+
+            foreach (string item in inventory)
+            {
+                Console.WriteLine(item);
+            }
+
+            inventory.Remove("health potion");
+            Console.WriteLine("inventory");
+            foreach (string item in inventory)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.ReadKey();
         }
     }
 }
